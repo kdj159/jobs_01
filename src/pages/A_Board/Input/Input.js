1f30ddafaad41a87a581694192ee8ac9b2efe2ea
kdj159 <kdj159@gmail.com>
2022-05-27 14:45:41 +0900

    테스트 메뉴 추가

---
+++ src/pages/A_Board/Input/Input.js
@@ -0,0 +1,29 @@
+import './Input.css';
+
+function Input() {
+  return (
+      <table className="Input">
+          <tr>
+              <th>a</th>
+              <th>b</th>
+          </tr>
+          <tr>
+              <th>c1</th>
+              <th>c2</th>
+          </tr>
+      </table>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
